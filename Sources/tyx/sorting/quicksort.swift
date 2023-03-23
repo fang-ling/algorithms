e62a2f7836f599c3d72ba18763eb3282e2844dab
Fang Ling <alice@jxx.world>
2023-03-23 16:47:21 +0800

    [quicksort] (refact) Array -> ArraySlice

--- Sources/tyx/sorting/quicksort.swift
+++ Sources/tyx/sorting/quicksort.swift
@@ -11,8 +11,9 @@ import Foundation
 /// `A[p ..< q]` and `A[q + 1 ... r]` such that each element of `A[p ..< q]` is
 /// less than or equal to `A[q]`, which is, in turn, less than or equal to each
 /// element of `A[q + 1 ... r]`.
-private
-func partition<T: Comparable>(array: inout [T], _ p : Int, _ r : Int) -> Int {
+private func partition<T: Comparable>(array: inout ArraySlice<T>,
+                                      _ p : Int,
+                                      _ r : Int) -> Int {
     let x = array[r]
     var i = p - 1
     for j in p ..< r {
@@ -30,7 +31,7 @@ func partition<T: Comparable>(array: inout [T], _ p : Int, _ r : Int) -> Int {
 /// exchanging element `A[r]` with an element chosen at random from `A[p ... r]`
 /// .
 private
-func randomized_partition<T : Comparable> (array: inout [T],
+func randomized_partition<T : Comparable> (array: inout ArraySlice<T>,
                                            _ p : Int,
                                            _ r : Int) -> Int {
     let i = Int.random(in: p ... r)
@@ -41,7 +42,9 @@ func randomized_partition<T : Comparable> (array: inout [T],
 
 /// - Note: Sort the two subarrays `A[p ..< q]` and `A[q + 1 ... r]` by
 /// recursive calls to quicksort.
-private func quicksort<T : Comparable>(array: inout [T], _ p : Int, _ r : Int) {
+private func quicksort<T : Comparable>(array: inout ArraySlice<T>,
+                                       _ p : Int,
+                                       _ r : Int) {
     if p < r {
         let q = randomized_partition(array: &array, p, r)
         quicksort(array: &array, p, q - 1)
@@ -55,6 +58,6 @@ extension Array where Element : Comparable {
     /// - Complexity: O(*n* log *n*) expected, where *n* is the length of the
     /// collection.
     public mutating func quicksort() {
-        tyx.quicksort(array: &self, 0, count - 1)
+        tyx.quicksort(array: &self[...], 0, count - 1)
     }
 }
